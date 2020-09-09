e8c81895350350b4e2000b471dbfcb5b12ead42b
OneOneLiu <1183418633@qq.com>
2020-09-09 08:55:05 +0800

    昨天遗漏的一些文件

---
+++ validate/cornell_pro.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 21 16:32:03 2020
+
+@author: LiuDahui
+"""
+
+
+import torch
+import glob
+import os
+import numpy as np
+
+from .grasp_pro import Grasps
+from .image_pro import Image,DepthImage
+
+
+class Cornell(torch.utils.data.Dataset):
+    #载入cornell数据集的类
+    def __init__(self,file_dir,include_depth=True,include_rgb=True,start = 0.0,end = 1.0,output_size = 300):
+        '''
+        :功能               : 数据集封装类的初始化函数，功能包括数据集读取，数据集划分，其他参数初始化等
+        :参数 file_dir      : str,按照官方文档的示例和之前的经验，这里需要读入数据集，所以需要指定数据的存放路径
+        :参数 include_depth : bool,是否包含depth图像输入
+        :参数 include_rgb   : bool,是否包含rgb图像输入
+        :参数 output_size   : 各图片的输出大小，裁剪得到
+        :参数 start,end     : float,为了方便数据集的拆分，这里定义添加两个边界参数start,end
+        :返回 None
+        ''' 
+        super(Cornell,self).__init__()
+        
+        #一些参数的传递
+        self.include_depth = include_depth
+        self.include_rgb = include_rgb
+        self.output_size = output_size
+        #去指定路径载入数据集数据
+        graspf = glob.glob(os.path.join(file_dir,'*','pcd*cpos.txt'))
+        graspf.sort()
+        
+        
+        l = len(graspf)
+        if l == 0:
+            raise FileNotFoundError('没有查找到数据集，请检查路径{}'.format(file_dir))
+        
+        rgbf = [filename.replace('cpos.txt','r.png') for filename in graspf]
+        depthf = [filename.replace('cpos.txt','d.tiff') for filename in graspf]
+        
+        #按照设定的边界参数对数据进行划分并指定为类的属性
+        self.graspf = graspf[int(l*start):int(l*end)]
+        self.rgbf = rgbf[int(l*start):int(l*end)]
+        self.depthf = depthf[int(l*start):int(l*end)]
+    @staticmethod
+    def numpy_to_torch(s):
+        '''
+        :功能     :将输入的numpy数组转化为torch张量，并指定数据类型，如果数据没有channel维度，就给它加上这个维度
+        :参数 s   :numpy ndarray,要转换的数组
+        :返回     :tensor,转换后的torch张量
+        '''
+        if len(s.shape) == 2:
+            return torch.from_numpy(np.expand_dims(s, 0).astype(np.float32))
+        else:
+            return torch.from_numpy(s.astype(np.float32))
+    
+    def _get_crop_attrs(self,idx):
+        '''
+        :功能     :读取多抓取框中心点的坐标，并结合output_size计算要裁剪的左上角点坐标
+        :参数 idx :int,
+        :返回     :计算出来的多抓取框中心点坐标和裁剪区域左上角点坐标
+        '''
+        grasp_rectangles = Grasps.load_from_cornell_files(self.graspf[idx])
+        center = grasp_rectangles.center
+        #按照ggcnn里面的话，这里本该加个限制条件，防止角点坐标溢出边界，但前面分析过，加不加区别不大，就不加了
+        #分析错误，后面出现bug了，所以还是加上吧
+        left = max(0, min(center[0] - self.output_size // 2, 640 - self.output_size))
+        top = max(0, min(center[1] - self.output_size // 2, 480 - self.output_size))
+        
+        return center,left,top
+    
+    def get_rgb(self,idx):
+        '''
+        :功能     :读取返回指定id的rgb图像
+        :参数 idx :int,要读取的数据id
+        :返回     :ndarray,处理好后的rgb图像
+        '''
+        rgb_img = Image.from_file(self.rgbf[idx])
+        rgb_img.normalize()
+        center,left,top = self._get_crop_attrs(idx)
+        rgb_img.crop((left,top),(left+self.output_size,top+self.output_size))
+        rgb_img.resize((self.output_size, self.output_size))
+        
+        return rgb_img.img
+    
+    #因为有时候只输入RGB三通道信息，所以，定义两个返回函数，一个读取RGB一个读取深度
+    def get_depth(self,idx):
+        '''
+        :功能     :读取返回指定id的depth图像
+        :参数 idx :int,要读取的数据id
+        :返回     :ndarray,处理好后的depth图像
+        '''
+        #目前这个DepthImage类还没有定义，后面仿照Image类给它定义一下
+        depth_img = DepthImage.from_file(self.depthf[idx])
+        depth_img.normalize()
+        center,left,top = self._get_crop_attrs(idx)
+        depth_img.crop((left,top),(left+self.output_size,top+self.output_size))
+        depth_img.resize((self.output_size, self.output_size))
+        return depth_img.img
+    
+    def get_grasp(self,idx):
+        '''
+        :功能       :读取返回指定id的抓取标注参数并将多个抓取框的参数返回融合
+        :参数 idx   :int,要读取的数据id
+        :参数 pos   :bool,是否生成返回位置映射图
+        :参数 angle :bool,是否生成返回角度映射图
+        :参数 width :bool,是否生成返回夹爪宽度映射图
+        :返回       :以图片的方式返回定义一个抓取的多个参数，包括中心点，角度，宽度和长度，同时返回idx，方便后面validate的时候查找真实的抓取框用
+        '''
+        grs = Grasps.load_from_cornell_files(self.graspf[idx])
+        grs.offset((-(grs.center[0]-self.output_size//2),-(grs.center[1]-self.output_size//2)))
+        
+        pos_img,angle_img,width_img = grs.generate_img(shape = (self.output_size,self.output_size))
+        
+        return pos_img,angle_img,width_img
+    def __getitem__(self,idx):
+        # 载入深度图像
+        #print(idx)
+        if self.include_depth:
+            depth_img = self.get_depth(idx)
+            x = self.numpy_to_torch(depth_img)
+        # 载入rgb图像
+        if self.include_rgb:
+            rgb_img = self.get_rgb(idx)
+            #torch是要求channel-first的，检测一下，如果读进来的图片是channel-last就调整一下，ggcnn中目前我没看到在哪调整的，但肯定是做了的
+            if rgb_img.shape[2] == 3:
+                rgb_img = np.moveaxis(rgb_img,2,0)
+            x = self.numpy_to_torch(rgb_img)
+        if self.include_depth and self.include_rgb:#如果灰度信息和rgb信息都要的话，就把他们堆到一起构成一个四通道的输入，
+            x = self.numpy_to_torch(
+                np.concatenate(
+                    (np.expand_dims(depth_img,0),rgb_img),0
+                )
+            )
+            
+        # 载入抓取标注参数
+        pos_img,angle_img,width_img = self.get_grasp(idx)
+        # 处理一下角度信息，因为这个角度值区间比较大，不怎么好处理，所以用两个三角函数把它映射一下：
+        cos_img = self.numpy_to_torch(np.cos(2*angle_img))
+        sin_img = self.numpy_to_torch(np.sin(2*angle_img))
+        
+        pos_img = self.numpy_to_torch(pos_img)
+        
+        # 限定抓取宽度范围并将其映射到[0,1]
+        width_img = np.clip(width_img, 0.0, 150.0)/150.0
+        width_img = self.numpy_to_torch(width_img)
+        
+        return x,(pos_img,cos_img,sin_img,width_img),idx
+    #映射类型的数据集，别忘了定义这个函数
+    def __len__(self):
+        return len(self.graspf)
